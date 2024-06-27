68204f71c4625470aad30d7701907fc0a56c6098
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2024-06-27 15:43:29 +0800

    Setup bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 
 export default function Home() {
-  return <h1>Hellow</h1>
+  return <div className="container">
+    <div className="row">
+      <div className="col-12 text-center">
+        <h1>hello</h1>
+      </div>
+    </div>
+  </div>
 }
